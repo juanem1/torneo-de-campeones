f555c546f66860e0e9e9a68c52efe521c8821524
Juane <juanemilioturk@gmail.com>
2014-06-27 10:51:53 -0300

    las vistas que faltaban

---
+++ app/views/tabla.blade.php
@@ -0,0 +1,108 @@
+@extends('layout')
+
+@section('content')
+	<!-- PAGE TITLE -->
+	<div class="page-title">Results</div>
+	<!-- PAGE CONTAINER -->
+	<section class="pagecontainer">
+		<div id="tabs-1">
+			<ul class="resp-tabs-list">
+				<li>League Table</li>
+				<li>Champions League</li>
+			</ul>
+			<div class="resp-tabs-container">
+				<div>
+					<table id="tablepress-1" class="tablepress tablepress-id-1 tablepress-responsive-phone">
+						<thead>
+							<tr class="row-1 odd">
+								<th class="column-1"><div>TEAMS</div></th><th class="column-2"><div>P</div></th><th class="column-3"><div>W</div></th><th class="column-4"><div>D</div></th><th class="column-5"><div>L</div></th><th class="column-6"><div>PTS</div></th>
+							</tr>
+						</thead>
+						<tbody class="row-hover">
+							<tr class="row-2 even">
+								<td class="column-1">Bayern Munich</td><td class="column-2">34</td><td class="column-3">29</td><td class="column-4">3</td><td class="column-5">1</td><td class="column-6">90</td>
+							</tr>
+							<tr class="row-3 odd">
+								<td class="column-1">Borussia Dortmund</td><td class="column-2">34</td><td class="column-3">27</td><td class="column-4">5</td><td class="column-5">3</td><td class="column-6">86</td>
+							</tr>
+							<tr class="row-4 even">
+								<td class="column-1">Schalke 04</td><td class="column-2">34</td><td class="column-3">26</td><td class="column-4">3</td><td class="column-5">5</td><td class="column-6">81</td>
+							</tr>
+							<tr class="row-5 odd">
+								<td class="column-1">Bayer Leverkusen</td><td class="column-2">34</td><td class="column-3">27</td><td class="column-4">8</td><td class="column-5">4</td><td class="column-6">76</td>
+							</tr>
+							<tr class="row-6 even">
+								<td class="column-1">VfL Wolfsburg</td><td class="column-2">34</td><td class="column-3">22</td><td class="column-4">5</td><td class="column-5">8</td><td class="column-6">74</td>
+							</tr>
+							<tr class="row-7 odd">
+								<td class="column-1">Borussia Monchengladbach</td><td class="column-2">34</td><td class="column-3">21</td><td class="column-4">9</td><td class="column-5">6</td><td class="column-6">72</td>
+							</tr>
+							<tr class="row-8 even">
+								<td class="column-1">Mainz</td><td class="column-2">34</td><td class="column-3">19</td><td class="column-4">10</td><td class="column-5">9</td><td class="column-6">70</td>
+							</tr>
+							<tr class="row-9 odd">
+								<td class="column-1">FC Augsburg</td><td class="column-2">34</td><td class="column-3">16</td><td class="column-4">5</td><td class="column-5">11</td><td class="column-6">62</td>
+							</tr>
+							<tr class="row-10 even">
+								<td class="column-1">TSG Hoffenheim</td><td class="column-2">34</td><td class="column-3">14</td><td class="column-4">6</td><td class="column-5">12</td><td class="column-6">55</td>
+							</tr>
+							<tr class="row-11 odd">
+								<td class="column-1">Hannover 96</td><td class="column-2">34</td><td class="column-3">12</td><td class="column-4">8</td><td class="column-5">14</td><td class="column-6">50</td>
+							</tr>
+							<tr class="row-12 even">
+								<td class="column-1">Hertha Berlin</td><td class="column-2">34</td><td class="column-3">15</td><td class="column-4">9</td><td class="column-5">13</td><td class="column-6">48</td>
+							</tr>
+							<tr class="row-13 odd">
+								<td class="column-1">Werder Bremen</td><td class="column-2">34</td><td class="column-3">13</td><td class="column-4">11</td><td class="column-5">15</td><td class="column-6">45</td>
+							</tr>
+							<tr class="row-14 even">
+								<td class="column-1">Eintracht Frankfurt</td><td class="column-2">34</td><td class="column-3">8</td><td class="column-4">7</td><td class="column-5">17</td><td class="column-6">42</td>
+							</tr>
+							<tr class="row-15 odd">
+								<td class="column-1">SC Freiburg</td><td class="column-2">34</td><td class="column-3">9</td><td class="column-4">8</td><td class="column-5">21</td><td class="column-6">35</td>
+							</tr>
+							<tr class="row-16 even">
+								<td class="column-1">VfB Stuttgart</td><td class="column-2">34</td><td class="column-3">5</td><td class="column-4">5</td><td class="column-5">20</td><td class="column-6">33</td>
+							</tr>
+							<tr class="row-17 odd">
+								<td class="column-1">Hamburg SV</td><td class="column-2">34</td><td class="column-3">3</td><td class="column-4">9</td><td class="column-5">22</td><td class="column-6">27</td>
+							</tr>
+							<tr class="row-18 even">
+								<td class="column-1">Nurnberg</td><td class="column-2">34</td><td class="column-3">4</td><td class="column-4">5</td><td class="column-5">25</td><td class="column-6">25</td>
+							</tr>
+							<tr class="row-19 odd">
+								<td class="column-1">TSV Eintracht Braunschweig</td><td class="column-2">34</td><td class="column-3">2</td><td class="column-4">6</td><td class="column-5">27</td><td class="column-6">22</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div>
+					<table id="tablepress-2" class="tablepress tablepress-id-2 tablepress-responsive-phone">
+						<thead>
+							<tr class="row-1 odd">
+								<th class="column-1"><div>TEAMS</div></th><th class="column-2"><div>P</div></th><th class="column-3"><div>W</div></th><th class="column-4"><div>D</div></th><th class="column-5"><div>L</div></th><th class="column-6"><div>PTS</div></th>
+							</tr>
+						</thead>
+						<tbody class="row-hover">
+							<tr class="row-2 even">
+								<td class="column-1">Bayern Munich</td><td class="column-2">6</td><td class="column-3">5</td><td class="column-4">0</td><td class="column-5">1</td><td class="column-6">15</td>
+							</tr>
+							<tr class="row-3 odd">
+								<td class="column-1">Manchester City</td><td class="column-2">6</td><td class="column-3">5</td><td class="column-4">0</td><td class="column-5">1</td><td class="column-6">15</td>
+							</tr>
+							<tr class="row-4 even">
+								<td class="column-1">Victoria Plyzen</td><td class="column-2">6</td><td class="column-3">1</td><td class="column-4">0</td><td class="column-5">5</td><td class="column-6">3</td>
+							</tr>
+							<tr class="row-5 odd">
+								<td class="column-1">CSKA Moskow</td><td class="column-2">6</td><td class="column-3">1</td><td class="column-4">0</td><td class="column-5">5</td><td class="column-6">3</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="clear"></div>
+	<!-- FOOTER -->
+	@include('partials.footer')
+@stop
