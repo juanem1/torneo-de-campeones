68657196ed9706bbc5ed43e11c8eb7ac508ad1aa
Juane <juanemilioturk@gmail.com>
2014-06-27 10:51:42 -0300

    las vistas que faltaban

---
+++ app/views/fixture.blade.php
@@ -0,0 +1,203 @@
+@extends('layout')
+
+@section('content')
+	<!-- PAGE TITLE -->
+	<div class="page-title">Fixtures</div>
+	<!-- FEATURED IMAGE -->
+	<div class="page-img">
+		<img src="/img/photos/featured.jpg" alt="" />
+	</div>
+	<!-- PAGE CONTAINER -->
+	<section class="pagecontainer">
+		<!--vertical Tabs-->
+		<div id="verticalTab">
+			<ul class="resp-tabs-list">
+				<li>2013-2014</li>
+				<li>2012-2013</li>
+				<li>2011-2012</li>
+				<li>2010-2011</li>
+			</ul>
+			<div class="resp-tabs-container">
+				<div>
+					<div class="sidebarbox-title">
+						<h3>Fixture y Resultados / 2013-2014</h3>
+					</div>
+					<!-- TABLE -->
+					<div class="fixture-row">
+						<a href="#">
+							<div class="fixture-row-left">Consectetur FC<div>?</div></div>
+							<div class="fixture-row-right"><div>?</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Fabulas FC<div>1</div></div>
+							<div class="fixture-row-right"><div>3</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>4</div></div>
+							<div class="fixture-row-right"><div>1</div>Vidisse FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Elit FC<div>2</div></div>
+							<div class="fixture-row-right"><div>4</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>1</div></div>
+							<div class="fixture-row-right"><div>1</div>Domesticarum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Ullamco FC<div>6</div></div>
+							<div class="fixture-row-right"><div>1</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Incididunt FC<div>1</div></div>
+							<div class="fixture-row-right"><div>5</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>2</div></div>
+							<div class="fixture-row-right"><div>1</div>Pariatur FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>1</div></div>
+							<div class="fixture-row-right"><div>3</div>Mentitum FC</div>
+						</a>
+					</div>
+				</div>
+				<div>
+					<div class="sidebarbox-title">
+						<h3>Fixture and Results / 2012-2013</h3>
+					</div>
+					<!-- TABLE -->
+					<div class="fixture-row">
+						<a href="#">
+							<div class="fixture-row-left">Elit FC<div>2</div></div>
+							<div class="fixture-row-right"><div>4</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>1</div></div>
+							<div class="fixture-row-right"><div>1</div>Domesticarum FC</div>
+						</a>                            
+						<a href="#">
+							<div class="fixture-row-left">Consectetur FC<div>2</div></div>
+							<div class="fixture-row-right"><div>1</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Fabulas FC<div>1</div></div>
+							<div class="fixture-row-right"><div>3</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>4</div></div>
+							<div class="fixture-row-right"><div>3</div>Vidisse FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Ullamco FC<div>6</div></div>
+							<div class="fixture-row-right"><div>1</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>2</div></div>
+							<div class="fixture-row-right"><div>0</div>Mentitum FC</div>
+						</a>                            
+						<a href="#">
+							<div class="fixture-row-left">Incididunt FC<div>2</div></div>
+							<div class="fixture-row-right"><div>5</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>2</div></div>
+							<div class="fixture-row-right"><div>2</div>Pariatur FC</div>
+						</a>
+					</div>
+				</div>
+				<div>
+					<div class="sidebarbox-title">
+						<h3>Fixture and Results / 2011-2012</h3>
+					</div>
+					<!-- TABLE -->
+					<div class="fixture-row">
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>1</div></div>
+							<div class="fixture-row-right"><div>3</div>Mentitum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Consectetur FC<div>3</div></div>
+							<div class="fixture-row-right"><div>3</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Fabulas FC<div>2</div></div>
+							<div class="fixture-row-right"><div>3</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>4</div></div>
+							<div class="fixture-row-right"><div>1</div>Vidisse FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Ullamco FC<div>2</div></div>
+							<div class="fixture-row-right"><div>1</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Incididunt FC<div>1</div></div>
+							<div class="fixture-row-right"><div>3</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Elit FC<div>2</div></div>
+							<div class="fixture-row-right"><div>4</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>6</div></div>
+							<div class="fixture-row-right"><div>1</div>Domesticarum FC</div>
+						</a>                            
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>2</div></div>
+							<div class="fixture-row-right"><div>3</div>Pariatur FC</div>
+						</a>
+					</div>
+				</div>
+				<div>
+					<div class="sidebarbox-title">
+						<h3>Fixture and Results / 2010-2011</h3>
+					</div>
+					<!-- TABLE -->
+					<div class="fixture-row">
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>4</div></div>
+							<div class="fixture-row-right"><div>1</div>Vidisse FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Consectetur FC<div>3</div></div>
+							<div class="fixture-row-right"><div>2</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Fabulas FC<div>5</div></div>
+							<div class="fixture-row-right"><div>3</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Elit FC<div>2</div></div>
+							<div class="fixture-row-right"><div>4</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Incididunt FC<div>3</div></div>
+							<div class="fixture-row-right"><div>5</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>2</div></div>
+							<div class="fixture-row-right"><div>1</div>Pariatur FC</div>
+						</a>                            
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>5</div></div>
+							<div class="fixture-row-right"><div>1</div>Domesticarum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Ullamco FC<div>6</div></div>
+							<div class="fixture-row-right"><div>1</div>Voluptate Cillum FC</div>
+						</a>
+						<a href="#">
+							<div class="fixture-row-left">Voluptate Cillum FC<div>4</div></div>
+							<div class="fixture-row-right"><div>3</div>Mentitum FC</div>
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="clear"></div>
+	<!-- FOOTER -->
+	@include('partials.footer')
+@stop
