a880670f2541bcd403d7282d5ef489f31a8efb62
Juane <juanemilioturk@gmail.com>
2014-07-03 01:40:43 -0300

    conroler

--- app/routes.php
+++ app/routes.php
@@ -16,3 +16,25 @@ Route::get('fixture', array('as' => 'fixture', 'uses' => 'PageController@fixture
 Route::get('tabla', array('as' => 'tabla', 'uses' => 'PageController@tabla'));
 Route::get('galeria', array('as' => 'galeria', 'uses' => 'PageController@galeria'));
 Route::get('contacto', array('as' => 'contacto', 'uses' => 'PageController@contacto'));
+
+// Provincias
+/*Route::get('buenos-aires', array('uses' => 'ProvinciaController@buenosAires'));
+Route::get('tucuman', array('uses' => 'ProvinciaController@tucuman'));
+Route::get('cordoba', array('uses' => 'ProvinciaController@cordoba'));
+Route::get('catamarca', array('uses' => 'ProvinciaController@catamarca'));
+Route::get('santiago', array('uses' => 'ProvinciaController@santiago'));
+Route::get('jujuy', array('uses' => 'ProvinciaController@jujuy'));
+Route::get('el-nacional', array('uses' => 'ProvinciaController@elNacional'));
+*/
+
+Route::pattern('provincia', '(buenos-aires|tucuman|cordoba|catamarca|santiago|jujuy|el-nacional)');
+Route::pattern('seccion', '(equipos|fixture|posiciones)');
+
+Route::get('{provincia}', array('uses' => 'ProvinciaController@callProvincia'));
+Route::get('{provincia}/premier-league/{seccion}', array('uses' => 'ProvinciaController@callProvincia'));
+Route::get('{provincia}/serie-b/{seccion}', array('uses' => 'ProvinciaController@callProvincia'));
+Route::get('{provincia}/serie-c/{seccion}', array('uses' => 'ProvinciaController@callProvincia'));
+
+
+
+
