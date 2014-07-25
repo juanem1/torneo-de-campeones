e6cc9b09423b4eb1611f2e335449b79fa10a0d7d
Juane <juanemilioturk@gmail.com>
2014-07-25 09:05:10 -0300

    login

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -7,7 +7,7 @@ class AccountController extends BaseController {
 	public function login()	{
 		/*$user = new User;
 		$user->username = 'admin';
-		$user->password = Hash::make('Eq6st258');
+		$user->password = Hash::make('password');
 		$user->email = 'juanemilioturk@gmail.com';
 		$user->save();*/
 		$this->layout->content = View::make('account.login');
@@ -15,14 +15,32 @@ class AccountController extends BaseController {
 
 	public function authenticate() {
 
-		$username = Input::get('username');
-		$password = Input::get('password');
-		
-		if (Auth::attempt(array('username' => $username, 'password' => $password))) {
-			return Redirect::intended('login')->with('message', 'Login Failed');
+		$rules = array(
+			'username' => 'required',
+			'password' => 'required|alphaNum|min:3'
+		);
+
+		$validator = Validator::make(Input::all(), $rules);
+		if ($validator->fails()) {
+			return Redirect::to('login')
+				->withErrors($validator)
+				->withInput(Input::except('password'));
 		} else {
-			return Redirect::to('dashboard');
-		}
+			$username = Input::get('username');
+			$password = Input::get('password');
+			
+			if (Auth::attempt(array('username' => $username, 'password' => $password))) {
+				//return Redirect::intended('login')->with('message', 'Login Failed');
+				return Redirect::to('login')->withInput(Input::except('password'));
+			} else {
+				return Redirect::to('dashboard');
+			}
+		}		
+	}
+
+	public function logout() {
+		Auth::logout();
+		return Redirect::to('login');
 	}
 
 }
