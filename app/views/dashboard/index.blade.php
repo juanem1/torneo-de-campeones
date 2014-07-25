6b6a2179ae72528da6da5d6e2eb6721af74b6254
Juane <juanemilioturk@gmail.com>
2014-07-25 09:06:12 -0300

    dashboard

---
+++ app/views/dashboard/index.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+
+@section('content')
+	<div class="page-title">Dashboard</div>
+	<!-- PAGE CONTAINER -->
+	<section class="pagecontainer using-grid">
+		<div class="grid">
+			<div class="unit two-thirds">
+				
+			</div>
+		</div>
+	</section>
+	<div class="clear"></div>
+	<!-- FOOTER -->
+	@include('partials.footer')
+@stop
