3436de5cee789924297c0294684e765b77299f6e
Juane <juanemilioturk@gmail.com>
2014-09-03 16:27:18 -0300

    Nuevo Home

---
+++ app/views/partials/sponsors.blade.php
@@ -0,0 +1,30 @@
+<div class="logos-title">Sponsors</div>
+<section class="transparent-bg">
+	<div id="logos">
+		<div class="slide">
+			<a href="#">
+				<img src="/img/photos/logo.png" alt="" />
+			</a>
+		</div>
+		<div class="slide">
+			<a href="#">
+				<img src="/img/sponsors/ole.png" alt="Ole Sponsor" />
+			</a>
+		</div>
+		<div class="slide">
+			<a href="#">
+				<img src="/img/sponsors/gatorade.png" alt="Gatorade Sponsor" />
+			</a>
+		</div>
+		<div class="slide">
+			<a href="#">
+				<img src="/img/photos/logo.png" alt="" />
+			</a>
+		</div>
+		<div class="slide">
+			<a href="#">
+				<img src="/img/photos/logo.png" alt="" />
+			</a>
+		</div>
+	</div>
+</section>
