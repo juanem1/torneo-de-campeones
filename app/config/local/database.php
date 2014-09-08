f0b9bfe8240511f00f958fe313c1b46b908f6856
Juane <juanemilioturk@gmail.com>
2014-09-08 17:26:38 -0300

    Muchos cambios

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'default' => 'sqlite',
+	'default' => 'mysql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -44,10 +44,15 @@ return array(
 	|
 	*/
 	'connections' => array(
-		'sqlite' => array(
-			'driver'   => 'sqlite',
-			'database' => __DIR__.'/../../database/football.db',
-			'prefix'   => '',
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'football',
+			'username'  => 'homestead',
+			'password'  => 'secret',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
 		),
 	),
 
