5109027a3c6fc2e895ad96448447baf5509a0357
Juane <juanemilioturk@gmail.com>
2014-09-08 17:27:06 -0300

    Muchos cambios

--- app/routes.php
+++ app/routes.php
@@ -1,21 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+// Noticias
+/*['only' => ['index', 'show']]*/
+Route::resource('news', 'NewsController');
+Route::resource('homeslider', 'HomesliderController');
+
+
 Route::get('/', array('as' => 'home', 'uses' => 'PageController@home'));
 Route::get('torneo', array('as' => 'torneo', 'uses' => 'PageController@torneo'));
 Route::get('fixture', array('as' => 'fixture', 'uses' => 'PageController@fixture'));
 Route::get('tabla', array('as' => 'tabla', 'uses' => 'PageController@tabla'));
 Route::get('galeria', array('as' => 'galeria', 'uses' => 'PageController@galeria'));
-Route::get('contacto', array('as' => 'contacto', 'uses' => 'PageController@contacto'));
+// COntacto por provincia
+Route::get('{provincia}/contacto', array('as' => 'contacto', 'uses' => 'PageController@contacto'));
 
 // Provincias
 Route::get('buenos-aires', array('uses' => 'ProvinciaController@buenosAires'));
@@ -27,21 +24,13 @@ Route::get('jujuy', array('uses' => 'ProvinciaController@jujuy'));
 Route::get('el-nacional', array('uses' => 'ProvinciaController@elNacional'));
 Route::get('santa-fe', array('uses' => 'ProvinciaController@santaFe'));
 
-/*Route::pattern('provincia', '(buenos-aires|tucuman|cordoba|catamarca|santiago|jujuy|el-nacional)');
-Route::pattern('seccion', '(equipos|fixture|posiciones)');
-
-Route::get('{provincia}', array('uses' => 'ProvinciaController@callProvincia'));
-Route::get('{provincia}/premier-league/{seccion}', array('uses' => 'ProvinciaController@callProvincia'));
-Route::get('{provincia}/serie-b/{seccion}', array('uses' => 'ProvinciaController@callProvincia'));
-Route::get('{provincia}/serie-c/{seccion}', array('uses' => 'ProvinciaController@callProvincia'));
-
 // Login
 Route::get('login', array('as' => 'login', 'uses' => 'AccountController@login'));
 Route::get('logout', array('as' => 'logout', 'uses' => 'AccountController@logout'));
 Route::post('authenticate', array('uses' => 'AccountController@authenticate'));
 
 // Rutas solo para admins
-Route::group(array('before' => 'auth'), function() {
+//Route::group(array('before' => 'auth'), function() {
 	// Dashboard
     Route::get('dashboard', array('as' => 'dashboard', 'uses' => 'DashboardController@index'));
-});*/
+//});
