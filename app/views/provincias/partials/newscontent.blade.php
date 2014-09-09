a90fcb951abf25ef4979541c5c806316a68b8cc1
Juane <juanemilioturk@gmail.com>
2014-09-09 14:16:10 -0300

    DB

--- app/views/provincias/partials/newscontent.blade.php
+++ app/views/provincias/partials/newscontent.blade.php
@@ -3,7 +3,7 @@
 	<h1>Ultimas Novedades</h1><hr/>
 	@foreach($news as $new)
 		<article class="post">
-			<strong>{{ $new->title }}</strong>
+			<h2>{{ $new->title }}</h2>
 			<div class="body">
 				{{ $new->body }}
 			</div>
