1bc0703195b171cdff08a392f10f696e655b3d27
Juane <juanemilioturk@gmail.com>
2014-09-13 18:22:32 -0300

    patis

---
+++ app/views/provincias/partials/ultimospartidos.blade.php
@@ -0,0 +1,36 @@
+@include('provincias.partials.ultimospartidos')
+<section class="transparent-bg">
+	<div id="fixture">
+		<div class="slide-content">
+			<div class="match-results">
+				<span>Atlas</span>
+				<span class="score">? - ?</span>
+				<span>Salamanca</span>
+			</div>
+			<div class="match-place">
+				<span>12 de Julio de 2014 - 10:00 PM - Canchas Cacho</span>
+			</div>
+		</div>
+		<div class="slide-content">
+			<div class="match-results">
+				<span>Barcelona</span>
+				<span class="score">2 - 1</span>
+				<span>Borussia</span>
+			</div>
+			<div class="match-place">
+				<span>12 de Julio de 2014 - 10:00 PM - Canchas Cacho</span>
+			</div>
+		</div>
+		<div class="slide-content">
+			<div class="match-results">
+				<span>La Docta FC</span>
+				<span class="score">1 - 3</span>
+				<span>Cordoba FC</span>
+			</div>
+			<div class="match-place">
+				<span>12 de Julio de 2014 - 10:00 PM - Canchas Cacho</span>
+			</div>
+		</div>
+	</div>
+	<div class="clear"></div>
+</section>
