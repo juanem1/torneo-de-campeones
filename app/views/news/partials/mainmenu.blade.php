64acc6fbb2205563ecfc077d0058b5513ac7e2c4
Juane <juanemilioturk@gmail.com>
2014-09-15 10:07:05 -0300

    news menu

---
+++ app/views/news/partials/mainmenu.blade.php
@@ -0,0 +1,9 @@
+<section id="mainmenu-container">
+	<a class="toggleMenu" href="#">Menu</a>
+	<nav>
+		<ul id="mainmenu">
+			<li><a href="{{ url('news') }}"><span>Todas las Novedades</span></a></li>
+			<li><a href="{{ url('news/create') }}"><span>Nueva Novedad</span></a></li>
+		</ul>
+	</nav>
+</section>
