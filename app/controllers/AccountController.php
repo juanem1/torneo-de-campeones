11e8624889f1f459ca9a53301a52dd3833954fed
Juane <juanemilioturk@gmail.com>
2014-09-14 23:24:23 -0300

    eujas gorda

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -23,18 +23,40 @@ class AccountController extends BaseController {
 		} else {
 			$username = Input::get('username');
 			$password = Input::get('password');
-			
+
 			if (Auth::attempt(array('username' => $username, 'password' => $password))) {
-				return Redirect::back()->withInput()->withErrors('Login Failed');
+				return Redirect::intended('dashboard');
 			} else {
-				return Redirect::to('dashboard');
+				return Redirect::back()->withInput()->withErrors('Login Failed');
 			}
-		}		
+		}
 	}
 
 	public function logout() {
 		Auth::logout();
 		return Redirect::to('login');
 	}
+
+	public function edit()
+	{
+		$this->layout->content = View::make('account.edit');
+	}
+
+	public function update()
+	{
+		$rules = array('password' => 'required|confirmed');
+		$validator = Validator::make(Input::all(), $rules);
+		if ( $validator->fails() )
+		{
+			return Redirect::back()->withInput()->withErrors($validator->messages());
+		}
+
+		$user = User::find(Auth::id());
+		$user->password = Hash::make(Input::get('password'));
+		$user->save();
+
+		return Redirect::to('dashboard')->withMessage('Su contraseña se actualizo con éxito.');
+	}
+
 
 }
