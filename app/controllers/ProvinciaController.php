96618e5105a59c09972e25aa0c5fd897dfa6d182
Juane <juanemilioturk@gmail.com>
2014-09-16 17:36:53 -0300

    camios

--- app/controllers/ProvinciaController.php
+++ app/controllers/ProvinciaController.php
@@ -12,65 +12,74 @@ class ProvinciaController extends BaseController {
 	public function buenosAires()
 	{
 		$provincia = 'buenos-aires';
-		$news = News::whereIn('provincia_id', array(0, 1))->get();
-		$sliders = Homeslider::whereIn('provincia_id', array(0, 1))->get();
-		$this->layout->content = View::make('provincias.buenosaires.home', compact('provincia', 'sliders', 'news'));
+		$mainmenu = View::make('partials.mainmenus.buenosaires');
+		$news = News::whereIn('provincia_id', array(1, 2))->get();
+		$sliders = Homeslider::whereIn('provincia_id', array(1, 2))->get();
+		$ultimospartidos = Ultimospartidos::whereIn('provincia_id', array(1, 2))->get();
+		$this->layout->content = View::make('provincias', compact('provincia', 'sliders', 'news', 'mainmenu', 'ultimospartidos'));
 	}
 
 	public function catamarca()
 	{
 		$provincia = 'catamarca';
-		$news = News::whereIn('provincia_id', array(0, 2))->get();
-		$sliders = Homeslider::whereIn('provincia_id', array(0, 2))->get();
-		$this->layout->content = View::make('provincias.catamarca.home', compact('provincia', 'sliders', 'news'));
+		$mainmenu = View::make('partials.mainmenus.catamarca');
+		$news = News::whereIn('provincia_id', array(1, 3))->get();
+		$sliders = Homeslider::whereIn('provincia_id', array(1, 3))->get();
+		$this->layout->content = View::make('provincias', compact('provincia', 'sliders', 'news', 'mainmenu'));
 	}
 
 	public function cordoba()
 	{
 		$provincia = 'cordoba';
-		$news = News::whereIn('provincia_id', array(0, 3))->get();
-		$sliders = Homeslider::whereIn('provincia_id', array(0, 3))->get();
-		$this->layout->content = View::make('provincias.cordoba.home', compact('provincia', 'sliders', 'news'));
+		$mainmenu = View::make('partials.mainmenus.cordoba');
+		$news = News::whereIn('provincia_id', array(1, 4))->get();
+		$sliders = Homeslider::whereIn('provincia_id', array(1, 4))->get();
+		$this->layout->content = View::make('provincias', compact('provincia', 'sliders', 'news', 'mainmenu'));
 	}
 
 	public function elNacional()
 	{
 		$provincia = 'el-nacional';
-		$news = News::whereIn('provincia_id', array(0, 4))->get();
-		$sliders = Homeslider::whereIn('provincia_id', array(0, 4))->get();
-		$this->layout->content = View::make('provincias.elnacional.home', compact('provincia', 'sliders', 'news'));
+		$mainmenu = View::make('partials.mainmenus.elnacional');
+		$news = News::whereIn('provincia_id', array(1, 5))->get();
+		$sliders = Homeslider::whereIn('provincia_id', array(1, 5))->get();
+		$this->layout->content = View::make('provincias', compact('provincia', 'sliders', 'news', 'mainmenu'));
 	}
 
 	public function jujuy()
 	{
 		$provincia = 'jujuy';
-		$news = News::whereIn('provincia_id', array(0, 5))->get();
-		$sliders = Homeslider::whereIn('provincia_id', array(0, 5))->get();
-		$this->layout->content = View::make('provincias.jujuy.home', compact('provincia', 'sliders', 'news'));
+		$mainmenu = View::make('partials.mainmenus.jujuy');
+		$news = News::whereIn('provincia_id', array(1, 6))->get();
+		$sliders = Homeslider::whereIn('provincia_id', array(1, 6))->get();
+		$this->layout->content = View::make('provincias', compact('provincia', 'sliders', 'news', 'mainmenu'));
 	}
 
 	public function santaFe()
 	{
 		$provincia = 'santa-fe';
-		$news = News::whereIn('provincia_id', array(0, 6))->get();
-		$sliders = Homeslider::whereIn('provincia_id', array(0, 6))->get();
-		$this->layout->content = View::make('provincias.santafe.home', compact('provincia', 'sliders', 'news'));
+		$mainmenu = View::make('partials.mainmenus.santafe');
+		$news = News::whereIn('provincia_id', array(1, 7))->get();
+		$sliders = Homeslider::whereIn('provincia_id', array(1, 7))->get();
+		$this->layout->content = View::make('provincias', compact('provincia', 'sliders', 'news', 'mainmenu'));
 	}
 
 	public function santiago()
 	{
 		$provincia = 'santiago';
-		$news = News::whereIn('provincia_id', array(0, 7))->get();
-		$sliders = Homeslider::whereIn('provincia_id', array(0, 7))->get();
-		$this->layout->content = View::make('provincias.santiago.home', compact('provincia', 'sliders', 'news'));
+		$mainmenu = View::make('partials.mainmenus.santiago');
+		$news = News::whereIn('provincia_id', array(1, 8))->get();
+		$sliders = Homeslider::whereIn('provincia_id', array(1, 8))->get();
+		$this->layout->content = View::make('provincias', compact('provincia', 'sliders', 'news', 'mainmenu'));
 	}
 
 	public function tucuman()
 	{
 		$provincia = 'tucuman';
-		$news = News::whereIn('provincia_id', array(0, 8))->get();
-		$sliders = Homeslider::whereIn('provincia_id', array(0, 8))->get();
-		$this->layout->content = View::make('provincias.tucuman.home', compact('provincia', 'sliders', 'news'));
+		$mainmenu = View::make('partials.mainmenus.tucuman');
+		$news = News::whereIn('provincia_id', array(1, 9))->get();
+		$sliders = Homeslider::whereIn('provincia_id', array(1, 9))->get();
+		$this->layout->content = View::make('provincias', compact('provincia', 'sliders', 'news', 'mainmenu'));
 	}
 
 }
