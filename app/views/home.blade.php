9d6a76ffa0e12beba550012195e946fe586b71a3
Juane <juanemilioturk@gmail.com>
2014-09-16 11:25:42 -0300

    nuevo home

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -10,6 +10,7 @@
 		<!-- CSS FILES -->
 		<link href="/css/libraries.css" rel="stylesheet" type="text/css" />
 		<link href="/css/styles.css" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 		<script type="text/javascript" src="/js/modernizr.js"></script>
 		<!--[if lt IE 9]>
 			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
@@ -17,26 +18,40 @@
 	</head>
 	<body>
 		<!-- HEADER -->
-		<header class="headerBlack">
+		<header class="headerBlack container">
 			<div class="logo">
 				<div class="logoWrapper">
 					<div class="bienvenidos">
 						<img src="/img/home/bienvenidos.png" width="300" height="180" >
 					</div>
-					<div>
-						<div class="homeLogos">
-							<div class="row1">
-								<a class="homeLogo cordoba" href="{{ url('cordoba') }}" title="Córdoba"></a>
-								<a class="homeLogo tucuman" href="{{ url('tucuman') }}" title="Tucumán"></a>
-								<a class="homeLogo catamarca" href="{{ url('catamarca') }}" title="Catamarca"></a>
-								<a class="homeLogo jujuy" href="{{ url('jujuy') }}" title="Jujuy"></a>
-							</div>
-							<div class="row2">
-								<a class="homeLogo santiago" href="{{ url('santiago') }}" title="Santiago del Estero"></a>
-								<a class="homeLogo santafe" href="{{ url('santa-fe') }}" title="Santa Fe"></a>
-								<a class="homeLogo buenosAires" href="{{ url('buenos-aires') }}" title="Buenos Aires"></a>
-								<a class="homeLogo elnacional" href="{{ url('el-nacional') }}" title="El Nacional"></a>
-							</div>
+
+					<div class="row">
+						<div class="col-md-3 col-sm-6">
+							<a class="homeLogo cordoba" href="{{ url('cordoba') }}" title="Córdoba"></a>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<a class="homeLogo tucuman" href="{{ url('tucuman') }}" title="Tucumán"></a>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<a class="homeLogo catamarca" href="{{ url('catamarca') }}" title="Catamarca"></a>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<a class="homeLogo jujuy" href="{{ url('jujuy') }}" title="Jujuy"></a>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="col-md-3 col-sm-6">
+							<a class="homeLogo santiago" href="{{ url('santiago') }}" title="Santiago del Estero"></a>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<a class="homeLogo santafe" href="{{ url('santa-fe') }}" title="Santa Fe"></a>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<a class="homeLogo buenosAires" href="{{ url('buenos-aires') }}" title="Buenos Aires"></a>
+						</div>
+						<div class="col-md-3 col-sm-6">
+							<a class="homeLogo elnacional" href="{{ url('el-nacional') }}" title="El Nacional"></a>
 						</div>
 					</div>
 				</div>
@@ -46,8 +61,8 @@
 		<script type="text/javascript" src="/js/libraries.js"></script>
 		<script src="/js/custom.js" type="text/javascript"></script>
 		<script type="text/javascript">
-		var wHeight = $(window).height() / 2;
-		$('.headerBlack').css('margin-top', wHeight - 175);
+		//var wHeight = $(window).height() / 2;
+		//$('.headerBlack').css('margin-top', wHeight - 175);
 		</script>
 	</body>
 </html>
