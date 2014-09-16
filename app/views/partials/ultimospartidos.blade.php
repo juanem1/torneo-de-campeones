74575b83fd7f6912e562c39e2fe24b57a7252e5b
Juane <juanemilioturk@gmail.com>
2014-09-16 17:43:07 -0300

    camios

--- app/views/partials/ultimospartidos.blade.php
+++ app/views/partials/ultimospartidos.blade.php
@@ -1,19 +1,19 @@
-<section class="transparent-bg">
-	<div id="fixture">
-        @if ( isset($ultimospartidos) )
-            @foreach($ultimospartidos as $partido)
-                <div class="slide-content">
-                    <div class="match-results">
-                        <span>{{ $partido->team_a }}</span>
-                        <span class="score">{{ $partido->results }}</span>
-                        <span>{{ $partido->team_b }}</span>
+@if ( isset($ultimospartidos) )
+    <section class="transparent-bg">
+        <div id="fixture">
+                @foreach($ultimospartidos as $partido)
+                    <div class="slide-content">
+                        <div class="match-results">
+                            <span>{{ $partido->team_a }}</span>
+                            <span class="score">{{ $partido->results }}</span>
+                            <span>{{ $partido->team_b }}</span>
+                        </div>
+                        <div class="match-place">
+                            <span>{{ $partido->description }}</span>
+                        </div>
                     </div>
-                    <div class="match-place">
-                        <span>{{ $partido->description }}</span>
-                    </div>
-                </div>
-            @endforeach
-	    @endif
-	</div>
-	<div class="clear"></div>
-</section>
+                @endforeach
+        </div>
+        <div class="clear"></div>
+    </section>
+@endif
