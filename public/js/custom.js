d5ea26fc2315f92cfec02c725d15b62bec7e4898
Juane <juanemilioturk@gmail.com>
2014-09-16 17:35:44 -0300

    camios

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,33 +1,33 @@
 /* MAIN MENU */
 var ww = document.body.clientWidth;
 jQuery(document).ready(function() {
-  "use strict";
-	jQuery('body').find("#mainmenu li a").each(function() {
-		if (jQuery(this).next().length > 0) {
-			jQuery(this).addClass("parent");
-		}
-	});
-	
-	jQuery('body').find(".toggleMenu").click(function(e) {
-		e.preventDefault();
-		jQuery(this).toggleClass("active");
-		jQuery('body').find("#mainmenu").toggle();
-	});
+    "use strict";
+    jQuery('body').find("#mainmenu li a").each(function() {
+        if (jQuery(this).next().length > 0) {
+            jQuery(this).addClass("parent");
+        }
+    });
+
+    jQuery('body').find(".toggleMenu").click(function(e) {
+        e.preventDefault();
+        jQuery(this).toggleClass("active");
+        jQuery('body').find("#mainmenu").toggle();
+    });
     adjustMenu();
 });
 jQuery(window).load(function() {
     "use strict";
     jQuery('body').backstretch("/img/photos/bg-blurred.jpg");
 });
-  
+
 jQuery(window).load(function () {
-     jQuery('body').find("#mainmenu").css('pointer-events', 'auto');
+    jQuery('body').find("#mainmenu").css('pointer-events', 'auto');
 });
 
 jQuery(window).bind('resize orientationchange', function() {
-  "use strict";
-	ww = document.body.clientWidth;
-	adjustMenu();
+    "use strict";
+    ww = document.body.clientWidth;
+    adjustMenu();
     if (ww < 900) {
         jQuery('body').find('#submenu').hide();
         jQuery('body').find('#submenu-login').hide();
@@ -39,36 +39,36 @@ jQuery(window).bind('resize orientationchange', function() {
 });
 
 var adjustMenu = function() {
-  "use strict";
-	if (ww < 900) {
-		jQuery('body').find(".toggleMenu").css("display", "inline-block");
-		if (!jQuery('body').find(".toggleMenu").hasClass("active")) {
-			jQuery('body').find("#mainmenu").hide();
-		} else {
-			jQuery('body').find("#mainmenu").show();
-		}
-		jQuery('body').find("#mainmenu li").unbind('mouseenter mouseleave');
-		jQuery('body').find("#mainmenu li a.parent").unbind('click').bind('click', function(e) {
-			e.preventDefault();
-			jQuery(this).parent("li").toggleClass("hover");
-		});
-	} else if (ww >= 900) {
-		jQuery('body').find(".toggleMenu").css("display", "none");
-		jQuery('body').find("#mainmenu").show();
-		jQuery('body').find("#mainmenu li").removeClass("hover");
-		jQuery('body').find("#mainmenu li a").unbind('click');
-		jQuery('body').find("#mainmenu li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
-		jQuery(this).toggleClass('hover');
-        jQuery(this).toggleClass('activelink');
-        jQuery(this).find("ul").toggleClass('animatedfast');
-        jQuery(this).find("ul").toggleClass('fadeInUp');
-		});
+    "use strict";
+    if (ww < 900) {
+        jQuery('body').find(".toggleMenu").css("display", "inline-block");
+        if (!jQuery('body').find(".toggleMenu").hasClass("active")) {
+            jQuery('body').find("#mainmenu").hide();
+        } else {
+            jQuery('body').find("#mainmenu").show();
+        }
+        jQuery('body').find("#mainmenu li").unbind('mouseenter mouseleave');
+        jQuery('body').find("#mainmenu li a.parent").unbind('click').bind('click', function(e) {
+            e.preventDefault();
+            jQuery(this).parent("li").toggleClass("hover");
+        });
+    } else if (ww >= 900) {
+        jQuery('body').find(".toggleMenu").css("display", "none");
+        jQuery('body').find("#mainmenu").show();
+        jQuery('body').find("#mainmenu li").removeClass("hover");
+        jQuery('body').find("#mainmenu li a").unbind('click');
+        jQuery('body').find("#mainmenu li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
+            jQuery(this).toggleClass('hover');
+            jQuery(this).toggleClass('activelink');
+            jQuery(this).find("ul").toggleClass('animatedfast');
+            jQuery(this).find("ul").toggleClass('fadeInUp');
+        });
         jQuery('body').find("#mainmenu ul li").unbind('mouseenter mouseleave').bind('mouseenter mouseleave', function() {
             jQuery(this).toggleClass('hover');
             jQuery(this).find("ul li").toggleClass('animatedfast');
             jQuery(this).find("ul li").toggleClass('fadeInLeft');
-		});
-	}
+        });
+    }
 };
 
 jQuery("body").find('#mobile-menu').on("click", function (e) {
@@ -89,7 +89,7 @@ jQuery(document).ready(function() {
             jQuery('.back-to-top').fadeOut(duration);
         }
     });
-    
+
     jQuery('.back-to-top').click(function(event) {
         event.preventDefault();
         jQuery('html, body').animate({scrollTop: 0}, duration);
@@ -117,5 +117,5 @@ jQuery(document).ready(function() {
 /////////////////* Info Boxes */////////////////////////
 
 jQuery('body').find('.message-close').on("click", function () {
-   jQuery(this).parent().fadeOut();
+    jQuery(this).parent().fadeOut();
 });
