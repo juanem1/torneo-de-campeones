46761bf2855c7971cf31f8b4f1d1d38c1c25fd33
Juane <juanemilioturk@gmail.com>
2014-09-18 11:59:56 -0300

    sacamos el mapa de contactos

--- app/views/contacto.blade.php
+++ app/views/contacto.blade.php
@@ -18,12 +18,11 @@
 				<p>Facebook Córdoba: <a href="https://www.facebook.com/torneodecampeonescdba" target="_blank">facebook.com/torneodecampeonescdba</a></p> 
 				<p>Facebook Catamarca: <a href="https://www.facebook.com/torneodecampeonescat" target="_blank">facebook.com/torneodecampeonescat</a></p> 
 
-				<h2>Canchas</h2>
+				<!--h2>Canchas</h2>
 				<p><strong>CANCHA 1:</strong> Voluptate se quorum pariatur, qui esse quae hic excepteur hic dolor illustriora ingeniis dolor iudicem, officia varias malis iis minim. Qui duis fore minim nescius an quis familiaritatem possumus sint nescius a lorem singulis domesticarum, quibusdam an legam cernantur.</p>
-				<!-- GOOGLE MAP -->
 				<div class="flex-video">
 					<iframe src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=tr&amp;geocode=&amp;q=new+york&amp;aq=&amp;sll=37.0625,-95.677068&amp;sspn=39.371738,86.572266&amp;ie=UTF8&amp;hq=&amp;hnear=New+York&amp;t=m&amp;z=10&amp;ll=40.714353,-74.005973&amp;output=embed&amp;iwloc=near"></iframe>
-				</div>
+				</div-->
 			</div>
 			<!-- RIGHT SIDE -->
 			<div class="unit one-third">
