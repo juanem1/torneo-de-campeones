574bfbea08854be3c8fbc118c5b028116741065e
Juane <juanemilioturk@gmail.com>
2014-12-02 18:57:31 -0300

    Nuevos links para el Nacional

--- app/views/partials/mainmenus/el-nacional.blade.php
+++ app/views/partials/mainmenus/el-nacional.blade.php
@@ -4,10 +4,38 @@
 		<ul id="mainmenu">
 			<li><a href="{{ url('el-nacional') }}"><span>Noticias</span></a></li>
 			<li><a href="/reglamento-torneo-de-campeones.pdf"><span>Reglamento</span></a></li>
-			<li><a href="#"><span>Equipos</span></a></li>
-			<li><a href="#"><span>Fixture</span></a></li>
-			<li><a href="#"><span>Posiciones</span></a></li>
-			<li><a href="#"><span>Goleadores</span></a></li>
+			<li>
+			    <a href="#"><span>Equipos</span></a>
+			    <ul class="sub-menu">
+                    <li><a target="_blank" href="http://www.enjore.com/es/team-list/30512/i-nacional-torneo-de-campeones-argentina-grupo-a/"><span>Grupo A</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/team-list/30513/i-nacional-torneo-de-campeones-argentina-grupo-b/"><span>Grupo B</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/team-list/30514/i-nacional-torneo-de-campeones-argentina-grupo-c/"><span>Grupo C</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/team-list/30515/i-nacional-torneo-de-campeones-argentina-grupo-d/"><span>Grupo D</span></a></li>
+                </ul>
+            </li>
+			<li>
+                <a href="#"><span>Fixture</span></a>
+                <ul class="sub-menu">
+                    <li><a target="_blank" href="http://www.enjore.com/es/calendar/30512/full/i-nacional-torneo-de-campeones-argentina-grupo-a/"><span>Grupo A</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/calendar/30513/full/i-nacional-torneo-de-campeones-argentina-grupo-b/"><span>Grupo B</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/calendar/30514/full/i-nacional-torneo-de-campeones-argentina-grupo-c/"><span>Grupo C</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/calendar/30515/full/i-nacional-torneo-de-campeones-argentina-grupo-d/"><span>Grupo D</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/calendar/30534/copa-de-oro/"><span>Copa de Oro</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/calendar/30535/copa-de-plata/"><span>Copa de Plata</span></a></li>
+                </ul>
+            </li>
+			<li>
+			    <a href="#"><span>Posiciones</span></a>
+			    <ul class="sub-menu">
+                    <li><a target="_blank" href="http://www.enjore.com/es/teamtable/30512/i-nacional-torneo-de-campeones-argentina-grupo-a/"><span>Grupo A</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/teamtable/30513/i-nacional-torneo-de-campeones-argentina-grupo-b/"><span>Grupo B</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/teamtable/30514/i-nacional-torneo-de-campeones-argentina-grupo-c/"><span>Grupo C</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/teamtable/30515/i-nacional-torneo-de-campeones-argentina-grupo-d/"><span>Grupo D</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/calendar/30534/copa-de-oro/"><span>Copa de Oro</span></a></li>
+                    <li><a target="_blank" href="http://www.enjore.com/es/calendar/30535/copa-de-plata/"><span>Copa de Plata</span></a></li>
+                </ul>
+			</li>
+			<li><a target="_blank" href="http://www.enjore.com/es/score/t-15280/i-nacional-torneo-de-campeones-argentina/"><span>Goleadores</span></a></li>
 			<li><a href="{{ url('el-nacional/contacto') }}"><span>Contacto</span></a></li>
 		</ul>
 	</nav>
